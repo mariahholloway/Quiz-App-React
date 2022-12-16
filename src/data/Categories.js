0111d6e23b8ca895f860ead27007e801e76a7961
ImaniJC <44530854+ImaniJC@users.noreply.github.com>
2022-12-15 20:20:33 -0500

    Update Categories.js

--- src/data/Categories.js
+++ src/data/Categories.js
@@ -1,15 +1,15 @@
 const Categories = [
   { category: "Films", value: 11 },
   { category: "Music", value: 12 },
-  { category: "Musicals and Theaters", value: 10 },
-  { category: "Television", value: 12 },
-  { category: "Video Games", value: 14 },
+  { category: "Musicals and Theaters", value: 13 },
+  { category: "Television", value: 14 },
+  { category: "Video Games", value: 15 },
   { category: "Board Games", value: 16 },
-  { category: "Sports", value: 18 },
-  { category: "Celebrities", value: 20 },
-  { category: "Comics", value: 22 },
-  { category: "Japanese Anime", value: 24 },
-  { category: "Cartoon and Animations", value: 26 },
+  { category: "Sports", value: 21 },
+  { category: "Celebrities", value: 26 },
+  { category: "Comics", value: 29 },
+  { category: "Japanese Anime", value: 31 },
+  { category: "Cartoon and Animations", value: 32 },
 ];
 
 export default Categories;
